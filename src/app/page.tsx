efcd3b7c2cad26d6534914da35044f1f5410871e
WilgotW <wilgotwardell@hotmail.com>
2024-10-27 15:45:31 +0100

    -

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,5 +3,8 @@ import HomeContent from "./components/HomeContent";
 
 export default async function Page() {
   const reviews = await getLatest(4);
+  console.log("Fetched reviews:", reviews);
   return <HomeContent reviews={reviews} />;
 }
+
+export const revalidate = 60;
